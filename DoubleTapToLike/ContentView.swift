70fe439e12c4468fff5a45b5dafb63d8b703b191
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-18 16:17:34 +0200

    Double Tap To Like

--- DoubleTapToLike/ContentView.swift
+++ DoubleTapToLike/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var ShowLike = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image("image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 400, height: 400)
+                    .onTapGesture(count: 2) {
+                        withAnimation {
+                            ShowLike.toggle()
+                            
+                   }
+                        DispatchQueue.main.asyncAfter(deadline: .now()+1) {
+                            withAnimation {
+                                ShowLike.toggle()
+                        }
+                    }
+                }
+                Image(systemName: "heart.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.red)
+                    .frame(width: 100, height: 100)
+                    .opacity(ShowLike ? 1: 0)
+                    .animation(Animation.linear(duration: 0.3))
+               }
+            }
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
