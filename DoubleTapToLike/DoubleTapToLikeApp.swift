92d7dc8e73fcd76bf05509b31aa056ef552ead2e
Ndayisenga Jean Claude <cloude056@yahoo.fr>
2021-05-18 15:58:56 +0200

    Initial Commit

---
+++ DoubleTapToLike/DoubleTapToLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleTapToLikeApp.swift
+//  DoubleTapToLike
+//
+//  Created by Ndayisenga Jean Claude on 18/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct DoubleTapToLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
